e618b67174d98861a7c4513709f5a3c8540246ef
Mack Octavian <maestrooctavian@gmail.com>
2024-07-04 17:56:09 +0300

    Added a dynamic segment

--- app/tickets/TicketList.js
+++ app/tickets/TicketList.js
@@ -1,35 +1,38 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 async function getTickets() {
-    const res = await fetch('http://localhost:4000/tickets', {
-        next: {
-            revalidate: 0
-        }
-    })
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 0,
+    },
+  });
 
-    return res.json()
+  return res.json();
 }
 
 async function TicketList() {
-    const tickets = await getTickets()
-    return (
-        <>
-            {
-                tickets.map(ticket => (
-                    <div key={ticket.id} className='card my-5'>
-                        <h3>{ticket.title}</h3>
-                        <p>{ticket.body.slice(0, 200)}...</p>
+  const tickets = await getTickets();
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
 
-                        <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
-                    </div>
-                ))
-            }
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
+        </div>
+      ))}
 
-            {tickets.length === 0 && (
-                <p className='text-center'> There are no open tickets, yay! </p>
-            )}
-        </>
-    )
+      {tickets.length === 0 && (
+        <p className="text-center"> There are no open tickets, yay! </p>
+      )}
+    </>
+  );
 }
 
-export default TicketList
+export default TicketList;
---
+++ app/tickets/[id]/page.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+async function getTicket(id) {
+  const res = await fetch("http://localhost:4000/tickets/" + id, {
+    next: {
+      revalidate: 60,
+    },
+  });
+
+  return res.json();
+}
+
+async function Ticket({ params }) {
+  const ticket = await getTicket(params.id);
+  return (
+    <div key={ticket.id} className="card my-5">
+      <h3>{ticket.title}</h3>
+      <p>{ticket.body.slice(0, 200)}...</p>
+
+      <div className={`pill ${ticket.priority}`}>
+        {ticket.priority} priority
+      </div>
+    </div>
+  );
+}
+
+export default Ticket;
