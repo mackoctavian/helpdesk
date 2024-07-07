48d8b1b7257bbafbc0ec4129c914bf81f55d516c
Mack Octavian <maestrooctavian@gmail.com>
2024-07-08 00:14:12 +0300

    Created route groups to tidy the app

---
+++ app/(auth)/signup/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+    return (
+        <main>
+            <h2 className='text-center'>Signup</h2>
+        </main>
+    )
+}
+
+export default Signup
