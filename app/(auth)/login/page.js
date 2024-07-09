39545353efb73cc518f9c4f961ff870b3d3cce53
Mack Octavian <maestrooctavian@gmail.com>
2024-07-09 23:54:07 +0300

    Added the auth forms for authentication

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,9 +1,17 @@
+"use client"
 import React from 'react'
+import AuthForm from '../AuthForm'
 
 function Login() {
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault()
+
+        console.log(email, password)
+    }
     return (
         <main>
             <h2 className='text-center'>Login</h2>
+            <AuthForm handleSubmit={handleSubmit} />
         </main>
     )
 }
--- app/(auth)/signup/page.js
+++ app/(auth)/signup/page.js
@@ -1,9 +1,19 @@
+"use client"
+
 import React from 'react'
+import AuthForm from '../AuthForm'
 
 function Signup() {
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault()
+
+        console.log("signup", email, password)
+    }
+
     return (
         <main>
             <h2 className='text-center'>Signup</h2>
+            <AuthForm handleSubmit={handleSubmit} />
         </main>
     )
 }
