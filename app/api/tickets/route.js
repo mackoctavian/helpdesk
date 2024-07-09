db0c2e2bfdd420227ed92b042fc2b983ad21f553
Mack Octavian <maestrooctavian@gmail.com>
2024-07-09 11:14:34 +0300

    Made the GET request handler dynamic

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server"
+
 export async function GET() {
-    const res = await fetch("http://localhost:4000/tickets")
+    const res = await fetch("http://localhost:4000/tickets", {
+        next: {
+            revalidate: 0
+        }
+    })
 
     const tickets = await res.json()
     return NextResponse.json(tickets, {
